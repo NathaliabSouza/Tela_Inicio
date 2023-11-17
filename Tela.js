848c224fec9481864ceb6532489c203df887bbcb
Nathalia B Souza <151152414+NathaliabSouza@users.noreply.github.com>
2023-11-17 08:59:24 -0300

    Update Tela.js

--- Tela.js
+++ Tela.js
@@ -1,1 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    alert("Bem-vindo à minha página!");
+    // Outras operações JavaScript podem ser realizadas aqui
+  });
+const links = document.querySelectorAll('.links a');
 
+links.forEach(link => {
+  link.addEventListener('mouseover', () => {
+    link.style.backgroundColor = '#423706';
+  });
+
+  link.addEventListener('mouseout', () => {
+    link.style.backgroundColor = '#060002';
+  });
+});
